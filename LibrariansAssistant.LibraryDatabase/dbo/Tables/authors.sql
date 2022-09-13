7033ed6d0d268f00048a74de4b71186a581ec48a
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-09-13 16:33:56 +0300

    feat(sql)!: add core tables to project

---
+++ LibrariansAssistant.LibraryDatabase/dbo/Tables/authors.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[authors]
+(
+	[id] INT NOT NULL IDENTITY, 
+    [first_name] NVARCHAR(25) NOT NULL, 
+    [last_name] NVARCHAR(50) NOT NULL, 
+    [patronymic] NVARCHAR(25) NULL, 
+    CONSTRAINT [PK_authors] PRIMARY KEY ([id]) 
+)
---
+++ LibrariansAssistant.LibraryDatabase/dbo/Tables/issuings.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[issuings]
+(
+	[id] INT NOT NULL IDENTITY, 
+    [reader_id] INT NOT NULL, 
+    [book_id] INT NOT NULL, 
+    [take_date] DATE NOT NULL, 
+    [returned] BIT NOT NULL, 
+    [return_date] DATE NULL, 
+    [return_state] INT NULL, 
+    CONSTRAINT [PK_issuings] PRIMARY KEY ([id]), 
+    CONSTRAINT [FK_issuings_readers] FOREIGN KEY ([reader_id]) REFERENCES [readers]([id]) ON DELETE CASCADE ON UPDATE CASCADE, 
+    CONSTRAINT [FK_issuings_books] FOREIGN KEY ([book_id]) REFERENCES [books]([id]) ON DELETE CASCADE ON UPDATE CASCADE
+)
