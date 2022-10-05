bc6619bf6c7568cb93c0d66e1d85c84f115703b9
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-10-05 17:02:07 +0300

    feat(infrastructure): add custom ORM mapping exception

---
+++ LibrariansAssistant.InfranstructureLayer/ObjectRelationalMappers/Exceptions/MappingException.cs
@@ -0,0 +1,11 @@
+﻿namespace LibrariansAssistant.InfranstructureLayer.ObjectRelationalMappers.Exceptions;
+
+[Serializable]
+public sealed class MappingException : Exception
+{
+    public MappingException() { }
+
+    public MappingException(string message) : base(message) { }
+
+    public MappingException(string message, Exception inner) : base(message, inner) { }
+}
