3924c5b00bf2a09573ef3aafa32ec83f3857df32
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-10-05 17:03:25 +0300

    feat(infrastructure): add ORM internal entities

---
+++ LibrariansAssistant.InfranstructureLayer/ObjectRelationalMappers/Implementations/Entities/PropertyDetails.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace LibrariansAssistant.InfranstructureLayer.ObjectRelationalMappers.Implementations.Entities;
+
+internal sealed class PropertyDetails
+{
+    internal PropertyInfo PropertyInfo { get; }
+
+    internal TypeDetails? TypeDetails { get; }
+
+    internal bool IsSimpleType { get; }
+
+    internal string SqlStyleName { get; }
+
+    public PropertyDetails(PropertyInfo propertyInfo, TypeDetails? typeDetails)
+    {
+        PropertyInfo = propertyInfo;
+        TypeDetails = typeDetails;
+
+        if (typeDetails is null)
+            IsSimpleType = true;
+
+        SqlStyleName =
+            string.Concat(propertyInfo.Name.Select(c => char.IsUpper(c) ? ("_" + c) : c.ToString()))
+            .TrimStart('_')
+            .ToLower();
+    }
+}
---
+++ LibrariansAssistant.InfranstructureLayer/ObjectRelationalMappers/Implementations/Entities/TypeDetails.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+namespace LibrariansAssistant.InfranstructureLayer.ObjectRelationalMappers.Implementations.Entities;
+
+internal sealed class TypeDetails
+{
+    internal Type ObjectType { get; }
+
+    internal bool IsEnumerable { get; }
+
+    internal IEnumerable<PropertyDetails> PropertyDetails { get; } = Enumerable.Empty<PropertyDetails>();
+
+    internal TypeDetails(Type type)
+    {
+        if ((type.IsGenericType is true) && type.GetGenericTypeDefinition().Equals(typeof(IEnumerable<>)))
+        {
+            ObjectType = type.GetGenericArguments()[0];
+            IsEnumerable = true;
+        }
+        else
+            ObjectType = type;
+
+        foreach (PropertyInfo propertyInfo in ObjectType.GetProperties())
+        {
+            TypeDetails? typeDetails = null;
+
+            if (IsSimpleType(propertyInfo.PropertyType) is false)
+                typeDetails = new TypeDetails(propertyInfo.PropertyType);
+
+            PropertyDetails = PropertyDetails.Append(new PropertyDetails(propertyInfo, typeDetails));
+        }
+    }
+
+    private static bool IsSimpleType(Type type) =>
+        (type.IsPrimitive is true) ||
+        type.Equals(typeof(string)) ||
+        type.Equals(typeof(decimal)) ||
+        type.Equals(typeof(DateTime)) ||
+        ((type.IsGenericType is true) && type.GetGenericTypeDefinition().Equals(typeof(Nullable<>)));
+}
