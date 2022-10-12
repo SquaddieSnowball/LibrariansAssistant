c34d872aa88addb7e0b60d768f880ea1543f0577
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-10-12 16:59:49 +0300

    feat(infrastructure): add SQL Server repository factory implementation

---
+++ LibrariansAssistant.InfranstructureLayer/RepositoryFactories/Implementations/SqlServerRepositoryFactory.cs
@@ -0,0 +1,12 @@
+﻿using LibrariansAssistant.InfranstructureLayer.Repositories.Implementations;
+using LibrariansAssistant.InfranstructureLayer.Repositories.Interfaces;
+
+namespace LibrariansAssistant.InfranstructureLayer.RepositoryFactories.Implementations;
+
+public sealed class SqlServerRepositoryFactory : IRepositoryFactory
+{
+    private SqlServerRepository? _sqlServerRepository;
+
+    public IRepository GetRepository() =>
+        _sqlServerRepository ??= new SqlServerRepository();
+}
