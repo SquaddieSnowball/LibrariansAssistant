dab83a2abf14246d522ac48317d6e4941e32428d
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-10-13 16:55:09 +0300

    feat(infrastructure): add SQL Server initialization string builder implementation

---
+++ LibrariansAssistant.InfranstructureLayer/Repositories/InitializationStringBuilders/Implementations/Entities/SqlServerIsbSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace LibrariansAssistant.InfranstructureLayer.Repositories.InitializationStringBuilders.Implementations.Entities;
+
+public sealed class SqlServerIsbSettings
+{
+    public string ServerName { get; set; }
+
+    public string ServerInstanceName { get; set; }
+
+    public bool UseWindowsAuthentication { get; set; } = true;
+
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? DatabaseName { get; set; }
+
+    public SqlServerIsbSettings(string serverName, string serverInstanceName) =>
+        (ServerName, ServerInstanceName) = (serverName, serverInstanceName);
+}
---
+++ LibrariansAssistant.InfranstructureLayer/Repositories/InitializationStringBuilders/Implementations/SqlServerInitializationStringBuilder.cs
@@ -0,0 +1,34 @@
+﻿using LibrariansAssistant.InfranstructureLayer.Repositories.InitializationStringBuilders.Implementations.Entities;
+using Microsoft.Data.SqlClient;
+
+namespace LibrariansAssistant.InfranstructureLayer.Repositories.InitializationStringBuilders.Implementations;
+
+public sealed class SqlServerInitializationStringBuilder : IInitializationStringBuilder
+{
+    private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new();
+
+    public SqlServerInitializationStringBuilder(SqlServerIsbSettings sqlServerIsbSettings)
+    {
+        if (sqlServerIsbSettings is null)
+            throw new ArgumentNullException(nameof(sqlServerIsbSettings),
+                "SQL Server initialization string builder settings must not be null.");
+
+        _sqlConnectionStringBuilder["Data Source"] =
+            (string.IsNullOrEmpty(sqlServerIsbSettings.ServerInstanceName) is true) ?
+            sqlServerIsbSettings.ServerName :
+            sqlServerIsbSettings.ServerName + "\\" + sqlServerIsbSettings.ServerInstanceName;
+
+        if (sqlServerIsbSettings.UseWindowsAuthentication is true)
+            _sqlConnectionStringBuilder["Integrated Security"] = true;
+        else
+        {
+            _sqlConnectionStringBuilder["User ID"] = sqlServerIsbSettings.UserName;
+            _sqlConnectionStringBuilder["Password"] = sqlServerIsbSettings.Password;
+        }
+
+        _sqlConnectionStringBuilder["Initial Catalog"] = sqlServerIsbSettings.DatabaseName;
+    }
+
+    public string Build() =>
+        _sqlConnectionStringBuilder.ConnectionString;
+}
