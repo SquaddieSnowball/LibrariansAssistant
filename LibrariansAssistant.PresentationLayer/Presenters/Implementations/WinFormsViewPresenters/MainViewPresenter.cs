6ca68dc6b1f1c1f8382acd24c06e7730b34d0ed5
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-10-14 17:56:12 +0300

    feat(presentation): add WinForms main view presenter implementation

---
+++ LibrariansAssistant.PresentationLayer/Presenters/Implementations/WinFormsViewPresenters/MainViewPresenter.cs
@@ -0,0 +1,22 @@
+﻿using LibrariansAssistant.InfranstructureLayer.Repositories.Interfaces;
+using LibrariansAssistant.PresentationLayer.ViewInterfaces.WinFormsViewInterfaces;
+using LibrariansAssistant.PresentationLayer.ViewServiceInterfaces;
+using LibrariansAssistant.ServicesLayer.CommonServices.Interfaces;
+
+namespace LibrariansAssistant.PresentationLayer.Presenters.Implementations.WinFormsViewPresenters;
+
+public sealed class MainViewPresenter : IPresenter
+{
+    private readonly IMainView _mainView;
+    private readonly IRepository _repository;
+    private readonly IDataAnnotationModelValidationService _dataAnnotationModelValidationService;
+    private readonly IMessageService _messageService;
+
+    public MainViewPresenter(IMainView mainView, IRepository repository,
+        IDataAnnotationModelValidationService dataAnnotationModelValidationService, IMessageService messageService) =>
+        (_mainView, _repository, _dataAnnotationModelValidationService, _messageService) =
+        (mainView, repository, dataAnnotationModelValidationService, messageService);
+
+    public void RunView() =>
+        _mainView.Show();
+}
