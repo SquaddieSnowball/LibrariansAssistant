879f790b3a70f2d48d471d520e117f1000981bf6
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-10-14 18:45:43 +0300

    feat(ui): add WinForms default message service

---
+++ LibrariansAssistant.UserInterfaceLayer/Services/WinFormsServices/Implementations/DefaultMessageService.cs
@@ -0,0 +1,15 @@
+﻿using LibrariansAssistant.PresentationLayer.ViewServiceInterfaces;
+
+namespace LibrariansAssistant.UserInterfaceLayer.Services.WinFormsServices.Implementations;
+
+internal class DefaultMessageService : IMessageService
+{
+    public void ShowMessage(string message) =>
+        MessageBox.Show(message, "Message", MessageBoxButtons.OK);
+
+    public void ShowWarning(string warning) =>
+        MessageBox.Show(warning, "Warning", MessageBoxButtons.OK);
+
+    public void ShowError(string error) =>
+        MessageBox.Show(error, "Error", MessageBoxButtons.OK);
+}
