67d530a226309a3d6d9d01f9e3cdebc0a2113ab8
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-10-28 18:35:13 +0300

    feat(ui): add Add/Edit item view

---
+++ LibrariansAssistant.UserInterfaceLayer/Views/WinFormsViews/AddEditItemView.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace LibrariansAssistant.UserInterfaceLayer.Views.WinFormsViews
+{
+    partial class AddEditItemView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBoxFields = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanelFields = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanelActions = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonConfirm = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.groupBoxFields.SuspendLayout();
+            this.tableLayoutPanelActions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.tableLayoutPanelMain.ColumnCount = 1;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.Controls.Add(this.groupBoxFields, 0, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.tableLayoutPanelActions, 0, 1);
+            this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelMain.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 2;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(382, 123);
+            this.tableLayoutPanelMain.TabIndex = 0;
+            // 
+            // groupBoxFields
+            // 
+            this.groupBoxFields.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.groupBoxFields.Controls.Add(this.tableLayoutPanelFields);
+            this.groupBoxFields.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxFields.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.groupBoxFields.Location = new System.Drawing.Point(20, 15);
+            this.groupBoxFields.Margin = new System.Windows.Forms.Padding(20, 15, 20, 10);
+            this.groupBoxFields.Name = "groupBoxFields";
+            this.groupBoxFields.Padding = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.groupBoxFields.Size = new System.Drawing.Size(342, 38);
+            this.groupBoxFields.TabIndex = 0;
+            this.groupBoxFields.TabStop = false;
+            this.groupBoxFields.Text = "Item action name";
+            // 
+            // tableLayoutPanelFields
+            // 
+            this.tableLayoutPanelFields.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.tableLayoutPanelFields.ColumnCount = 2;
+            this.tableLayoutPanelFields.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelFields.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelFields.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelFields.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.tableLayoutPanelFields.Location = new System.Drawing.Point(3, 20);
+            this.tableLayoutPanelFields.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanelFields.Name = "tableLayoutPanelFields";
+            this.tableLayoutPanelFields.Padding = new System.Windows.Forms.Padding(3);
+            this.tableLayoutPanelFields.RowCount = 1;
+            this.tableLayoutPanelFields.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelFields.Size = new System.Drawing.Size(336, 15);
+            this.tableLayoutPanelFields.TabIndex = 0;
+            // 
+            // tableLayoutPanelActions
+            // 
+            this.tableLayoutPanelActions.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.tableLayoutPanelActions.ColumnCount = 2;
+            this.tableLayoutPanelActions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelActions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelActions.Controls.Add(this.buttonConfirm, 0, 0);
+            this.tableLayoutPanelActions.Controls.Add(this.buttonCancel, 1, 0);
+            this.tableLayoutPanelActions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelActions.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.tableLayoutPanelActions.Location = new System.Drawing.Point(3, 66);
+            this.tableLayoutPanelActions.Name = "tableLayoutPanelActions";
+            this.tableLayoutPanelActions.RowCount = 1;
+            this.tableLayoutPanelActions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelActions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelActions.Size = new System.Drawing.Size(376, 54);
+            this.tableLayoutPanelActions.TabIndex = 1;
+            // 
+            // buttonConfirm
+            // 
+            this.buttonConfirm.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.buttonConfirm.AutoSize = true;
+            this.buttonConfirm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.buttonConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonConfirm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.buttonConfirm.Location = new System.Drawing.Point(84, 7);
+            this.buttonConfirm.Margin = new System.Windows.Forms.Padding(3, 3, 10, 10);
+            this.buttonConfirm.Name = "buttonConfirm";
+            this.buttonConfirm.Size = new System.Drawing.Size(94, 32);
+            this.buttonConfirm.TabIndex = 0;
+            this.buttonConfirm.Text = "Confirm";
+            this.buttonConfirm.UseVisualStyleBackColor = false;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.buttonCancel.AutoSize = true;
+            this.buttonCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.buttonCancel.Location = new System.Drawing.Point(198, 7);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(10, 3, 3, 10);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(94, 32);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            // 
+            // AddEditItemView
+            // 
+            this.AcceptButton = this.buttonConfirm;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(382, 123);
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddEditItemView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add/Edit item";
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.groupBoxFields.ResumeLayout(false);
+            this.tableLayoutPanelActions.ResumeLayout(false);
+            this.tableLayoutPanelActions.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanelMain;
+        private GroupBox groupBoxFields;
+        private TableLayoutPanel tableLayoutPanelActions;
+        private Button buttonConfirm;
+        private Button buttonCancel;
+        private TableLayoutPanel tableLayoutPanelFields;
+    }
+}
---
+++ LibrariansAssistant.UserInterfaceLayer/Views/WinFormsViews/AddEditItemView.cs
@@ -0,0 +1,42 @@
+﻿namespace LibrariansAssistant.UserInterfaceLayer.Views.WinFormsViews;
+
+internal sealed partial class AddEditItemView : Form
+{
+    public string FieldsTitle
+    {
+        get => groupBoxFields.Text;
+        set => groupBoxFields.Text = value;
+    }
+
+    internal AddEditItemView()
+    {
+        InitializeComponent();
+    }
+
+    internal AddEditItemView AddField(string name, Control control)
+    {
+        var labelName = new Label()
+        {
+            Anchor = AnchorStyles.Left,
+            Margin = new Padding(30, 0, 0, 0),
+            Text = name
+        };
+
+        control.Anchor = AnchorStyles.Left;
+
+        int maxHeight = (labelName.Height > control.Height) ? labelName.Height : control.Height;
+
+        Height += maxHeight + 15;
+
+        tableLayoutPanelFields.RowStyles.Insert(
+            tableLayoutPanelFields.RowStyles.Count - 1,
+            new RowStyle(SizeType.Absolute, maxHeight + 15));
+
+        tableLayoutPanelFields.Controls.Add(labelName, 0, tableLayoutPanelFields.RowCount - 1);
+        tableLayoutPanelFields.Controls.Add(control, 1, tableLayoutPanelFields.RowCount - 1);
+
+        tableLayoutPanelFields.RowCount++;
+
+        return this;
+    }
+}
