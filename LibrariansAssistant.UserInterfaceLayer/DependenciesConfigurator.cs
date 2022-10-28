7f26e0c63d9461205e67857a7541726882f43703
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-10-28 18:23:36 +0300

    feat(ui): add new dependencies to configurator

--- LibrariansAssistant.UserInterfaceLayer/DependenciesConfigurator.cs
+++ LibrariansAssistant.UserInterfaceLayer/DependenciesConfigurator.cs
@@ -3,6 +3,12 @@ using LibrariansAssistant.DomainModelLayer.Models.Author;
 using LibrariansAssistant.DomainModelLayer.Models.Book;
 using LibrariansAssistant.DomainModelLayer.Models.Issuing;
 using LibrariansAssistant.DomainModelLayer.Models.Reader;
+using LibrariansAssistant.ServicesLayer.ModelServices.Author;
+using LibrariansAssistant.ServicesLayer.ModelServices.Book;
+using LibrariansAssistant.ServicesLayer.ModelServices.Issuing;
+using LibrariansAssistant.ServicesLayer.ModelServices.Reader;
+using LibrariansAssistant.UserInterfaceLayer.Services.CommonServices.Implementations;
+using LibrariansAssistant.UserInterfaceLayer.Services.CommonServices.Interfaces;
 
 namespace LibrariansAssistant.UserInterfaceLayer;
 
@@ -14,5 +20,10 @@ internal static class DependenciesConfigurator
         DependenciesContainer.Register<IBookModel, BookModel>();
         DependenciesContainer.Register<IIssuingModel, IssuingModel>();
         DependenciesContainer.Register<IReaderModel, ReaderModel>();
+        DependenciesContainer.Register<IAuthorService, AuthorService>();
+        DependenciesContainer.Register<IBookService, BookService>();
+        DependenciesContainer.Register<IIssuingService, IssuingService>();
+        DependenciesContainer.Register<IReaderService, ReaderService>();
+        DependenciesContainer.Register<IApplicationConfigurationService, ApplicationConfigurationService>();
     }
 }
