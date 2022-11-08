207af203273568fce1a09c87677e495ec9a5c7cb
Andrei Malashenko <97245660+SquaddieSnowball@users.noreply.github.com>
2022-11-08 17:58:08 +0300

    fix(ui): rework Settings view

--- LibrariansAssistant.UserInterfaceLayer/Views/WinFormsViews/SettingsView.cs
+++ LibrariansAssistant.UserInterfaceLayer/Views/WinFormsViews/SettingsView.cs
@@ -1,4 +1,11 @@
 ﻿using LibrariansAssistant.DependenciesLayer;
+using LibrariansAssistant.InfranstructureLayer.Repositories.InitializationStringBuilders;
+using LibrariansAssistant.InfranstructureLayer.Repositories.InitializationStringBuilders.Implementations;
+using LibrariansAssistant.InfranstructureLayer.Repositories.InitializationStringBuilders.Implementations.Entities;
+using LibrariansAssistant.InfranstructureLayer.Repositories.Interfaces;
+using LibrariansAssistant.InfranstructureLayer.RepositoryFactories;
+using LibrariansAssistant.InfranstructureLayer.RepositoryFactories.Implementations;
+using LibrariansAssistant.UserInterfaceLayer.Entities;
 using LibrariansAssistant.UserInterfaceLayer.Entities.WinFormsEntities;
 using LibrariansAssistant.UserInterfaceLayer.Helpers.WinFormsHelpers.ColorTables;
 using LibrariansAssistant.UserInterfaceLayer.Services.CommonServices.Interfaces;
@@ -8,23 +15,32 @@ namespace LibrariansAssistant.UserInterfaceLayer.Views.WinFormsViews;
 
 internal sealed partial class SettingsView : Form
 {
+    private const int FormDefaultHeight = 195;
     private readonly IApplicationConfigurationService _applicationConfigurationService =
         DependenciesContainer.Resolve<IApplicationConfigurationService>()!;
-    private readonly WinFormsDefaultMessageService _winFormsDefaultMessageService = new();
+    private readonly WinFormsMessageService _winFormsMessageService = new();
     private readonly Dictionary<SettingsGroup, Control[]> _settingsControls = new();
-    private readonly Dictionary<string, string> _changedSettings = new();
-    private readonly int _formDefaultHeight = 195;
+    private readonly Dictionary<string, object?> _modifiedSettings = new();
+    private readonly RepositoryType _selectedRepositoryType = RepositoryType.SqlServer;
     private SettingsGroup _selectedSettingsGroup;
+
+    internal IRepository? Repository { get; private set; }
+
+    internal string? InitializationString { get; private set; }
+
+    internal bool SettingCreateEmptyDatabase { get; private set; }
 
     internal SettingsView()
     {
         InitializeComponent();
         SubsribeToControlEvents();
         InitializeView();
+        UpdatePropertyValues();
     }
 
     private void SubsribeToControlEvents()
     {
+        FormClosing += SettingsViewOnFormClosing;
         connectionToolStripMenuItem.Click += ConnectionToolStripMenuItemOnClick;
         additionalToolStripMenuItem.Click += AdditionalToolStripMenuItemOnClick;
         buttonApply.Click += ButtonApplyOnClick;
@@ -40,6 +56,63 @@ internal sealed partial class SettingsView : Form
         SwitchSettingsGroupView(SettingsGroup.Connection);
     }
 
+    private void UpdatePropertyValues()
+    {
+        IInitializationStringBuilder initializationStringBuilder;
+        IRepositoryFactory repositoryFactory;
+
+        switch (_selectedRepositoryType)
+        {
+            case RepositoryType.SqlServer:
+
+                var sqlServerIsbSettings = new SqlServerIsbSettings(
+                    (_applicationConfigurationService.GetSettingValue("SqlServer_ServerName") as string)!,
+                    (_applicationConfigurationService.GetSettingValue("SqlServer_ServerInstanceName") as string)!)
+                {
+                    UseWindowsAuthentication =
+                        (bool)_applicationConfigurationService.GetSettingValue("SqlServer_UseWindowsAuthentication")!,
+                    UserName = _applicationConfigurationService.GetSettingValue("SqlServer_UserName") as string,
+                    Password = _applicationConfigurationService.GetSettingValue("SqlServer_Password") as string
+                };
+
+                initializationStringBuilder = new SqlServerInitializationStringBuilder(sqlServerIsbSettings);
+                InitializationString = initializationStringBuilder.Build();
+
+                repositoryFactory = new SqlServerRepositoryFactory();
+                Repository = repositoryFactory.GetRepository();
+
+                break;
+            default:
+                break;
+        }
+
+        SettingCreateEmptyDatabase = (bool)_applicationConfigurationService.GetSettingValue("CreateEmptyDatabase")!;
+    }
+
+    private void SettingsViewOnFormClosing(object? sender, FormClosingEventArgs e)
+    {
+        if (_modifiedSettings.Any() is true)
+        {
+            DialogResult dialogResult = MessageBox.Show(
+                "You have unsaved changes." + Environment.NewLine + "Do you want to save them before leaving?",
+                "Warnig", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+
+            switch (dialogResult)
+            {
+                case DialogResult.Yes:
+                    SaveSettings();
+                    break;
+                case DialogResult.No:
+                    break;
+                case DialogResult.Cancel:
+                    e.Cancel = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
     private void ConnectionToolStripMenuItemOnClick(object? sender, EventArgs e) =>
         SwitchSettingsGroupView(SettingsGroup.Connection);
 
@@ -48,144 +121,59 @@ internal sealed partial class SettingsView : Form
 
     private void ButtonApplyOnClick(object? sender, EventArgs e)
     {
-        try
-        {
-            _applicationConfigurationService.UpdateAppSettings(_changedSettings);
-
-            _changedSettings.Clear();
-
-            UpdateApplyButtonState();
-        }
-        catch (Exception ex)
-        {
-            _winFormsDefaultMessageService.ShowError(ex.Message);
-        }
-    }
-
-    private void UpdateSetting(string name, string value)
-    {
-        if (_changedSettings.ContainsKey(name) is true)
-            _changedSettings[name] = value;
-        else
-            _changedSettings.Add(name, value);
-
-        UpdateApplyButtonState();
-    }
-
-    private void UpdateApplyButtonState()
-    {
-        if (_changedSettings.Any() is true)
-            buttonApply.ForeColor = Color.FromArgb(230, 230, 230);
-        else
-            buttonApply.ForeColor = Color.FromArgb(130, 130, 130);
-    }
-
-    private void SwitchSettingsGroupView(SettingsGroup newSettingsGroup)
-    {
-        if (newSettingsGroup == _selectedSettingsGroup)
+        if (_modifiedSettings.Any() is false)
             return;
 
-        foreach (object? toolStripMenuItem in menuStripSettings.Items)
-            ((ToolStripMenuItem)toolStripMenuItem).BackColor = Color.FromArgb(30, 30, 30);
-
-        switch (newSettingsGroup)
-        {
-            case SettingsGroup.Connection:
-                connectionToolStripMenuItem.BackColor = Color.FromArgb(70, 70, 70);
-                break;
-            case SettingsGroup.Additional:
-                additionalToolStripMenuItem.BackColor = Color.FromArgb(70, 70, 70);
-                break;
-            default:
-                throw new NotImplementedException("This setting group view is not yet implemented.");
-        }
-
-        if (_selectedSettingsGroup is not 0)
-            tableLayoutPanelSettings.Controls.CopyTo(_settingsControls[_selectedSettingsGroup], 0);
-
-        tableLayoutPanelSettings.Controls.Clear();
-        tableLayoutPanelSettings.RowStyles.Clear();
-
-        Height = _formDefaultHeight;
-        groupBoxSettings.Text = newSettingsGroup.ToString() + " settings";
-
-        foreach (Control newControl in _settingsControls[newSettingsGroup])
-        {
-            Height += newControl.Height + 20;
-            newControl.Dock = DockStyle.Top;
-
-            tableLayoutPanelSettings.RowStyles.Add(new RowStyle(SizeType.Absolute, newControl.Height + 20));
-            tableLayoutPanelSettings.Controls.Add(newControl);
-
-            tableLayoutPanelSettings.RowCount++;
-        }
-
-        _selectedSettingsGroup = newSettingsGroup;
+        SaveSettings();
     }
 
     private Control[] GenerateConnectionSettingsControls()
     {
-        TextBox textBoxServerName = CreateTextBox();
-        TextBox textBoxServerInstanceName = CreateTextBox();
-        CheckBox checkBoxUseWindowsAuthentication = CreateCheckBox("Use Windows authentication");
-        TextBox textBoxUsername = CreateTextBox();
-        TextBox textBoxPassword = CreateTextBox();
+        TextBox textBoxSqlServerServerName = CreateTextBox();
+        TextBox textBoxSqlServerServerInstanceName = CreateTextBox();
+        CheckBox checkBoxSqlServerUseWindowsAuthentication = CreateCheckBox("Use Windows authentication");
+        TextBox textBoxSqlServerUsername = CreateTextBox();
+        TextBox textBoxSqlServerPassword = CreateTextBox();
 
-        textBoxUsername.Enabled = false;
-        textBoxUsername.BackColor = Color.FromArgb(35, 35, 35);
-        textBoxUsername.ForeColor = Color.FromArgb(130, 130, 130);
-        textBoxPassword.Enabled = false;
-        textBoxPassword.BackColor = Color.FromArgb(35, 35, 35);
-        textBoxPassword.ForeColor = Color.FromArgb(130, 130, 130);
-        textBoxPassword.PasswordChar = '*';
+        textBoxSqlServerPassword.PasswordChar = '*';
 
-        textBoxServerName.TextChanged += (sender, e) =>
-            UpdateSetting("Server name", textBoxServerName.Text);
+        textBoxSqlServerServerName.Text = _applicationConfigurationService.GetSettingValue("SqlServer_ServerName") as string;
+        textBoxSqlServerServerInstanceName.Text =
+            _applicationConfigurationService.GetSettingValue("SqlServer_ServerInstanceName") as string;
+        checkBoxSqlServerUseWindowsAuthentication.Checked =
+            (bool)_applicationConfigurationService.GetSettingValue("SqlServer_UseWindowsAuthentication")!;
+        textBoxSqlServerUsername.Text = _applicationConfigurationService.GetSettingValue("SqlServer_UserName") as string;
+        textBoxSqlServerPassword.Text = _applicationConfigurationService.GetSettingValue("SqlServer_Password") as string;
 
-        textBoxServerInstanceName.TextChanged += (sender, e) =>
-            UpdateSetting("Server instance name", textBoxServerInstanceName.Text);
+        UpdateSqlServerCredentialsState(checkBoxSqlServerUseWindowsAuthentication,
+            textBoxSqlServerUsername, textBoxSqlServerPassword);
 
-        checkBoxUseWindowsAuthentication.CheckedChanged += (sender, e) =>
+        textBoxSqlServerServerName.TextChanged += (sender, e) =>
+            UpdateSetting("SqlServer_ServerName", textBoxSqlServerServerName.Text);
+
+        textBoxSqlServerServerInstanceName.TextChanged += (sender, e) =>
+            UpdateSetting("SqlServer_ServerInstanceName", textBoxSqlServerServerInstanceName.Text);
+
+        checkBoxSqlServerUseWindowsAuthentication.CheckedChanged += (sender, e) =>
         {
-            UpdateSetting("Use Windows authentication", checkBoxUseWindowsAuthentication.Checked.ToString());
-
-            if (checkBoxUseWindowsAuthentication.Checked is true)
-            {
-                textBoxUsername.Enabled = true;
-                textBoxPassword.Enabled = true;
-
-                textBoxUsername.BackColor = Color.FromArgb(60, 60, 60);
-                textBoxPassword.BackColor = Color.FromArgb(60, 60, 60);
-
-                textBoxUsername.ForeColor = Color.FromArgb(230, 230, 230);
-                textBoxPassword.ForeColor = Color.FromArgb(230, 230, 230);
-            }
-            else
-            {
-                textBoxUsername.Enabled = false;
-                textBoxPassword.Enabled = false;
-
-                textBoxUsername.BackColor = Color.FromArgb(35, 35, 35);
-                textBoxPassword.BackColor = Color.FromArgb(35, 35, 35);
-
-                textBoxUsername.ForeColor = Color.FromArgb(130, 130, 130);
-                textBoxPassword.ForeColor = Color.FromArgb(130, 130, 130);
-            }
+            UpdateSetting("SqlServer_UseWindowsAuthentication", checkBoxSqlServerUseWindowsAuthentication.Checked);
+            UpdateSqlServerCredentialsState(checkBoxSqlServerUseWindowsAuthentication,
+                textBoxSqlServerUsername, textBoxSqlServerPassword);
         };
 
-        textBoxUsername.TextChanged += (sender, e) =>
-            UpdateSetting("User name", textBoxUsername.Text);
+        textBoxSqlServerUsername.TextChanged += (sender, e) =>
+            UpdateSetting("SqlServer_UserName", textBoxSqlServerUsername.Text);
 
-        textBoxPassword.TextChanged += (sender, e) =>
-            UpdateSetting("Password", textBoxPassword.Text);
+        textBoxSqlServerPassword.TextChanged += (sender, e) =>
+            UpdateSetting("SqlServer_Password", textBoxSqlServerPassword.Text);
 
         var controls = new List<Control>()
         {
-            CreateLabelControlTableLayoutPanel(CreateLabel("Server name:"), textBoxServerName),
-            CreateLabelControlTableLayoutPanel(CreateLabel("Server instance name:"), textBoxServerInstanceName),
-            CreateControlTableLayoutPanel(checkBoxUseWindowsAuthentication),
-            CreateLabelControlTableLayoutPanel(CreateLabel("User name:"), textBoxUsername),
-            CreateLabelControlTableLayoutPanel(CreateLabel("Password:"), textBoxPassword)
+            CreateLabelControlTableLayoutPanel(CreateLabel("Server name:"), textBoxSqlServerServerName),
+            CreateLabelControlTableLayoutPanel(CreateLabel("Server instance name:"), textBoxSqlServerServerInstanceName),
+            CreateControlTableLayoutPanel(checkBoxSqlServerUseWindowsAuthentication),
+            CreateLabelControlTableLayoutPanel(CreateLabel("User name:"), textBoxSqlServerUsername),
+            CreateLabelControlTableLayoutPanel(CreateLabel("Password:"), textBoxSqlServerPassword)
         };
 
         return controls.ToArray();
@@ -198,8 +186,11 @@ internal sealed partial class SettingsView : Form
 
         labelCreateEmptyDatabaseNote.Margin = new Padding(52, 0, 0, 0);
 
+        checkBoxCreateEmptyDatabase.Checked =
+            (bool)_applicationConfigurationService.GetSettingValue("CreateEmptyDatabase")!;
+
         checkBoxCreateEmptyDatabase.CheckedChanged += (sender, e) =>
-            UpdateSetting("Create empty database", checkBoxCreateEmptyDatabase.Checked.ToString());
+            UpdateSetting("CreateEmptyDatabase", checkBoxCreateEmptyDatabase.Checked);
 
         var controls = new List<Control>()
         {
@@ -274,4 +265,108 @@ internal sealed partial class SettingsView : Form
             Margin = new Padding(33, 0, 0, 0),
             Text = text
         };
+
+    private static void UpdateSqlServerCredentialsState(CheckBox checkBoxUseWindowsAuthentication,
+        TextBox textBoxUsername, TextBox textBoxPassword)
+    {
+        if (checkBoxUseWindowsAuthentication.Checked is true)
+        {
+            textBoxUsername.Enabled = false;
+            textBoxPassword.Enabled = false;
+
+            textBoxUsername.BackColor = Color.FromArgb(35, 35, 35);
+            textBoxPassword.BackColor = Color.FromArgb(35, 35, 35);
+
+            textBoxUsername.ForeColor = Color.FromArgb(130, 130, 130);
+            textBoxPassword.ForeColor = Color.FromArgb(130, 130, 130);
+        }
+        else
+        {
+            textBoxUsername.Enabled = true;
+            textBoxPassword.Enabled = true;
+
+            textBoxUsername.BackColor = Color.FromArgb(60, 60, 60);
+            textBoxPassword.BackColor = Color.FromArgb(60, 60, 60);
+
+            textBoxUsername.ForeColor = Color.FromArgb(230, 230, 230);
+            textBoxPassword.ForeColor = Color.FromArgb(230, 230, 230);
+        }
+    }
+
+    private void UpdateApplyButtonState()
+    {
+        if (_modifiedSettings.Any() is true)
+            buttonApply.ForeColor = Color.FromArgb(230, 230, 230);
+        else
+            buttonApply.ForeColor = Color.FromArgb(130, 130, 130);
+    }
+
+    private void SwitchSettingsGroupView(SettingsGroup newSettingsGroup)
+    {
+        if (newSettingsGroup == _selectedSettingsGroup)
+            return;
+
+        foreach (object? toolStripMenuItem in menuStripSettings.Items)
+            ((ToolStripMenuItem)toolStripMenuItem).BackColor = Color.FromArgb(30, 30, 30);
+
+        switch (newSettingsGroup)
+        {
+            case SettingsGroup.Connection:
+                connectionToolStripMenuItem.BackColor = Color.FromArgb(70, 70, 70);
+                break;
+            case SettingsGroup.Additional:
+                additionalToolStripMenuItem.BackColor = Color.FromArgb(70, 70, 70);
+                break;
+            default:
+                break;
+        }
+
+        if (_selectedSettingsGroup is not 0)
+            tableLayoutPanelSettings.Controls.CopyTo(_settingsControls[_selectedSettingsGroup], 0);
+
+        tableLayoutPanelSettings.Controls.Clear();
+        tableLayoutPanelSettings.RowStyles.Clear();
+
+        Height = FormDefaultHeight;
+        groupBoxSettings.Text = newSettingsGroup.ToString() + " settings";
+
+        foreach (Control newControl in _settingsControls[newSettingsGroup])
+        {
+            Height += newControl.Height + 20;
+            newControl.Dock = DockStyle.Top;
+
+            tableLayoutPanelSettings.RowStyles.Add(new RowStyle(SizeType.Absolute, newControl.Height + 20));
+            tableLayoutPanelSettings.Controls.Add(newControl);
+
+            tableLayoutPanelSettings.RowCount++;
+        }
+
+        _selectedSettingsGroup = newSettingsGroup;
+    }
+
+    private void UpdateSetting(string name, object? value)
+    {
+        if (_modifiedSettings.ContainsKey(name) is true)
+            _modifiedSettings[name] = value;
+        else
+            _modifiedSettings.Add(name, value);
+
+        UpdateApplyButtonState();
+    }
+
+    private void SaveSettings()
+    {
+        try
+        {
+            _applicationConfigurationService.UpdateSettingsValues(_modifiedSettings);
+            _modifiedSettings.Clear();
+
+            UpdatePropertyValues();
+            UpdateApplyButtonState();
+        }
+        catch (Exception ex)
+        {
+            _winFormsMessageService.ShowError(ex.Message);
+        }
+    }
 }
